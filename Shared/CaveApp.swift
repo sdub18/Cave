3030a321d35dc0f406f2a46c2f0a83acb3095dbf
sdubois18 <samdubois18@icloud.com>
2021-01-30 21:41:17 -0500

    Initial Files and such

---
+++ Shared/CaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaveApp.swift
+//  Shared
+//
+//  Created by Sam DuBois on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct CaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
